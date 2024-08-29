4f573d2e8db2c24087fc5410f1a85d6b80722ccd
Rafael Parrela de Oliveira <RafaelParrela2005@hotmail.com>
2024-08-29 14:39:42 +0000

    Aplicação que soma dois números inteiros

---
+++ OlaMundo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num1, num2, result;
+        System.out.println("Informe o primeiro número: ");
+        num1 = teclado.nextInt();
+        System.out.println("Informe o segundo número: ");
+        num2 = teclado.nextInt();
+
+        result = num1 + num2;
+
+        System.out.println("Resultado: " + result);
+        System.exit(0);
+    }
+}
