c1fd386ea6d70baca65ce855e153b78a9c50d5ac
Rafael Parrela de Oliveira <rafaelparrela2005@hotmail.com>
2024-09-17 15:43:11 -0300

    Rafael Parrela

--- ConversorDolarRealA.java
+++ ConversorDolarRealA.java
@@ -4,18 +4,14 @@ public class ConversorDolarRealA {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar a cotação do dólar em reais
         System.out.print("Digite a cotação do dólar em reais: ");
         double cotacaoDolar = scanner.nextDouble();
 
-        // Solicitar o valor que o usuário possui em dólares
         System.out.print("Digite o valor que você possui em dólares: ");
         double valorEmDolares = scanner.nextDouble();
 
-        // Converter o valor em dólares para reais
         double valorEmReais = valorEmDolares * cotacaoDolar;
 
-        // Exibir o resultado
         System.out.println("O valor em reais é: " + valorEmReais);
 
         scanner.close();
--- ConversorDolarRealB.java
+++ ConversorDolarRealB.java
@@ -4,18 +4,14 @@ public class ConversorDolarRealB{
 
     public static void main(String[] args) {
 
-        // Solicitar a cotação do dólar em reais usando JOptionPane
         String cotacaoDolarString = JOptionPane.showInputDialog("Digite a cotação do dólar em reais:");
         double cotacaoDolar = Double.parseDouble(cotacaoDolarString);
 
-        // Solicitar o valor que o usuário possui em dólares
         String valorEmDolaresString = JOptionPane.showInputDialog("Digite o valor que você possui em dólares:");
         double valorEmDolares = Double.parseDouble(valorEmDolaresString);
 
-        // Converter o valor em dólares para reais
         double valorEmReais = valorEmDolares * cotacaoDolar;
 
-        // Exibir o resultado com JOptionPane
         JOptionPane.showMessageDialog(null, String.format("O valor em reais é: " + valorEmReais + "R$."));
     }
 }
