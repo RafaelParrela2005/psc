ff9e8ddc881c10b5ef72e4be6c81bfd25cf063b0
Rafael Parrela de Oliveira <RafaelParrela2005@hotmail.com>
2024-10-09 20:05:30 -0300

    Update BuscaVetor1.java

--- BuscaVetor1.java
+++ BuscaVetor1.java
@@ -6,6 +6,7 @@ public class BuscaVetor1 {
         Scanner scanner = new Scanner(System.in);
         int[] vetor = new int[10];
 
+        // Solicita a digitação de 10 números e armazena eles no vetor.
         System.out.println("Digite 10 números:");
         for (int i = 0; i < vetor.length; i++) {
             System.out.print("Número " + (i + 1) + ": ");
@@ -14,6 +15,7 @@ public class BuscaVetor1 {
 
         boolean continuar = true;
 
+        //loop abaixo permite que o usuário escolha o algoritmo de busca a ser usado.
         while (continuar) {
             System.out.println("\nEscolha o algoritmo de busca:");
             System.out.println("1 - Busca Sequencial");
@@ -25,8 +27,9 @@ public class BuscaVetor1 {
             int numero = scanner.nextInt();
 
             int comparacoes = 0;
-            int posicao = -1;
+            int posicao = -1; // -1 é o padrão que significa Não Encontrado (caso seja retornado assim).
 
+            // Executa a busca com base no algoritmo selecionado.
             switch (opcao) {
                 case 1:
                     posicao = buscaSequencial(vetor, numero);
@@ -57,6 +60,7 @@ public class BuscaVetor1 {
         scanner.close();
     }
 
+    // Método para busca sequencial.
     public static int buscaSequencial(int[] vetor, int numero) {
         for (int i = 0; i < vetor.length; i++) {
             if (vetor[i] == numero) {
@@ -66,16 +70,18 @@ public class BuscaVetor1 {
         return -1;
     }
 
+    // Método para busca sequencial com sentinela.
     public static int buscaSequencialSentinela(int[] vetor, int numero) {
         int[] novoVetor = new int[vetor.length + 1];
-        System.arraycopy(vetor, 0, novoVetor, 0, vetor.length);
-        novoVetor[vetor.length] = numero;
+        System.arraycopy(vetor, 0, novoVetor, 0, vetor.length); // Copia os elementos do vetor original.
+        novoVetor[vetor.length] = numero; // Adiciona o número buscado como sentinela.
 
         int i = 0;
         while (novoVetor[i] != numero) {
             i++;
         }
 
+         // Verifica se a sentinela é o último elemento do vetor original.
         if (i == vetor.length) {
             return -1;
         } else {
