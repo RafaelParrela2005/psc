adb8a20f3f3bb7e5c6b46dc1d6d4afbcfe316687
Sinval Henrique Ferreira Lima <sinvals.henrique.lima@gmail.com>
2024-12-05 06:15:49 -0300

    Update Pessoa.java

--- Lista 14 - PSC/Questão 3/Pessoa.java
+++ Lista 14 - PSC/Questão 3/Pessoa.java
@@ -1,1 +1,19 @@
+package heranca;
 
+class Pessoa {
+    private String nome;
+
+    // Construtor
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    // Métodos de acesso
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
